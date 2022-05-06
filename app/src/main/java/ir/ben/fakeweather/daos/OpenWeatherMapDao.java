d64fc830c88bc14aaeaf93f3116836730a99b4e1
Mojtaba <rastegar123456789@gmail.com>
2022-05-06 11:29:32 +0430

    primary light theme added. an error in OpenWeatherMapDao has been fixed.

--- app/src/main/java/ir/ben/fakeweather/daos/OpenWeatherMapDao.java
+++ app/src/main/java/ir/ben/fakeweather/daos/OpenWeatherMapDao.java
@@ -1,5 +1,6 @@
 package ir.ben.fakeweather.daos;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -9,6 +10,7 @@ import java.util.List;
 import ir.ben.fakeweather.models.OpenWeatherMap;
 import ir.ben.fakeweather.models.Weather;
 
+@Dao
 public interface OpenWeatherMapDao {
     @Query("SELECT * FROM open_weather_map WHERE lat LIKE :lat AND lon LIKE :lon")
     OpenWeatherMap getOpenWeatherMapWithLatLong(double lat, double lon);
